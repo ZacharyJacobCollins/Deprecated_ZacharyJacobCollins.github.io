8f70a3d8791f52fed9909fce8af70b28db34f455
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-01-11 18:53:16 -0500

    changed index.js build

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../index.html'),
-    assetsRoot: path.resolve(__dirname, '/'),
+    assetsRoot: path.resolve(__dirname, '../'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
