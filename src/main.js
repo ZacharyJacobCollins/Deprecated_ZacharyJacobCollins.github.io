67e31cb20a81f272e2493a16c1c49108271fa68c
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-01-11 21:51:38 -0500

    not quite functioning anymore

--- src/main.js
+++ src/main.js
@@ -1,11 +1,43 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+//Template code below
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
+
+
+import Vue from 'vue'
+import App from './App.vue'
+// import store from './store'
+import router from './router'
+// import { sync } from 'vuex-router-sync'
+// import * as filters from './filters'
+
+// sync the router with the vuex store.
+// this registers `store.state.route`
+// sync(store, router)
+
+// register global utility filters.
+// Object.keys(filters).forEach(key => {
+//   Vue.filter(key, filters[key])
+// })
+
+// create the app instance.
+// here we inject the router and store to all child components,
+// making them available everywhere as `this.$router` and `this.$store`.
+const app = new Vue(Vue.util.extend({
+  router
+  // store
+}, App)).$mount('#app')
+
+// expose the app, the router and the store.
+// note we are not mounting the app here, since bootstrapping will be
+// different depending on whether we are in a browser or on the server.
+export { app, router} //, store }
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import Hello from '../components/Hello.vue'
+import WelcomeView from '../views/WelcomeView.vue'
+
+
+export default new Router({
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    { path: '/welcome', component: WelcomeView },
+    { path: '/', redirect: '/welcome' }
+  ]
+})
