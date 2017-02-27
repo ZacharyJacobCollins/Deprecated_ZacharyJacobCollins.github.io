c45efb2cc444dece73e9e8b782bbff6eb56edc2f
ZacharyJacobCollins <ZacharyJacobCollins@gmail.com>
2017-02-27 00:12:46 -0500

    added resume and such

---
+++ src/views/LearningModel.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="container">
+		<div class="projects-wrapper">
+			<!-- Row 1 -->
+			<div class="row">
+
+			</div>
+
+			<br />
+
+		</div>
+	</div>
+</template>
+
+<script>
+	import Projectcard from '../components/Projectcard.vue';
+
+	export default {
+	  name: 'projects-view',
+	  components: {
+	  	Projectcard
+	  }
+	}
+</script>
+
+<style scoped>
+
+	.projects-wrapper {
+		margin-top: 100px;
+	}
+
+</style>
---
+++ src/views/ResumeView.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="container">
+	<iframe src="https://resume.creddle.io/embed/edho0xwpy88" seamless></iframe> 
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'resume-view',
+	  components: {
+
+	  }
+	}
+</script>
+
+<style scoped>
+	html, body {
+		overflow: hidden;
+	}
+
+	.container, iframe {
+		 width: 850px;
+		 height: 1100px;
+	}
+
+</style>
